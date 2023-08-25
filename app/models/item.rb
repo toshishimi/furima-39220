3828c0756bbcc3dac77c73a2784a58bbc11c00be
toshishimi <sc.104.k@gmail.com>
2023-08-25 21:45:10 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,24 @@
 class Item < ApplicationRecord
 
+  validates :item_name,             presence: true
+  validates :item_info,             presence: true
+  validates :category_id,           numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_status_id,        numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_status_id,    numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,         numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,                 presence: true
+
+
+
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+  belongs_to :shipping_status
+
 end
