1a40e88da8166f7f4005c4a2567def5d690aa7d7
toshishimi <sc.104.k@gmail.com>
2023-08-26 00:09:44 +0900

    出品機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,37 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
 
   def index
-    
   end
   
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_info, :category_id, :item_status_id, :shipping_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+ 
+
 end
+# def move_to_index
+   # @prototype = Prototype.find(params[:id])
+    #@user = @prototype.user
+    #unless user_signed_in? && current_user == @user
+     # redirect_to root_path
+    #end
+  #end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-
+  validates :image,                 presence: true
   validates :item_name,             presence: true
   validates :item_info,             presence: true
   validates :category_id,           numericality: { other_than: 1 , message: "can't be blank"}
