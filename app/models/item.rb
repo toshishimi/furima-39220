b2621ca875560cf0740f571193203edc994756b1
toshishimi <sc.104.k@gmail.com>
2023-08-26 19:50:41 +0900

    単体テストコードを記入

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   validates :shipping_status_id,    numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id,         numericality: { other_than: 1 , message: "can't be blank"}
   validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price,                 presence: true
+  validates :price,                 numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
 
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+
+
+    item_name              { Faker::Name.name }
+    item_info              { Faker::Lorem.sentence }
+    category_id            { Faker::Number.between(from: 2, to: 11) }
+    item_status_id         { Faker::Number.between(from: 2, to: 7) }
+    shipping_status_id     { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id          { Faker::Number.between(from: 2, to: 48) }
+    scheduled_delivery_id  { Faker::Number.between(from: 2, to: 4) }
+    price                  { Faker::Number.between(from: 300, to: 9_999_999) }
+
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
