cb689ed9b65e546d8716a2e29c1f60440dab08b0
toshishimi <sc.104.k@gmail.com>
2023-08-31 21:16:41 +0900

    ルーティング、コントローラー、ビューの変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: :edit
+
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -22,7 +24,18 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
 
 
@@ -32,13 +45,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :item_info, :category_id, :item_status_id, :shipping_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    @item = Item.find(params[:id])
+    @user = @item.user
+    unless user_signed_in? && current_user == @user
+      redirect_to root_path
+    end
+  end
  
 
 end
-# def move_to_index
-   # @prototype = Prototype.find(params[:id])
-    #@user = @prototype.user
-    #unless user_signed_in? && current_user == @user
-     # redirect_to root_path
-    #end
-  #end
