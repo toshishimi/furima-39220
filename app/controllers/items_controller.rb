5aa0bb58e3167c5c9b6db3718932c3ca1a340ede
toshishimi <sc.104.k@gmail.com>
2023-09-01 01:13:34 +0900

    コントローラーを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,8 +37,12 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if item.user_id == current_user.id
+      item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
 
