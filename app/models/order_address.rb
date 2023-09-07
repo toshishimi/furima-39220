2af9e0cf8334c5ef6dea83d6dbf856d2c43b29c4
toshishimi <sc.104.k@gmail.com>
2023-09-08 00:48:23 +0900

    nameエラーのため修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -20,7 +20,7 @@ class OrderAddress
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
 
-    Address.create(
+    ShippingAddress.create(
       postcode: postcode, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
   end
 
