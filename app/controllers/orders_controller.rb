9bd6786549839c12347af011200cdba6216505d5
toshishimi <sc.104.k@gmail.com>
2023-09-07 21:17:28 +0900

    売却後の遷移ページを調整

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
-  before_action :non_purchased_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
 
 
   def index
@@ -36,9 +36,8 @@ class OrdersController < ApplicationController
     )
   end
 
-  def non_purchased_item
+  def set_item
     @item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 
 end
